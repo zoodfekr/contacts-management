d9fd8fc05cc0bb775452616055e4f8e14ed18252
ramin <zoodfekr.r@gmail.com>
2022-11-10 16:48:12 +0330

    usetransition

--- src/component/Accordion.jsx
+++ src/component/Accordion.jsx
@@ -1,41 +1,44 @@
+import { useMemo } from "react";
+import { useTransition } from "react";
 import { useEffect, useState } from "react";
+import Spiner from "./Preloader";
 
 const Accordion = () => {
+	let [input, setinput] = useState();
+	let [list, setlist] = useState();
 
-	const [input, setinput] = useState();
-	let data = [];
-
-	useEffect(() => {
-		if (input) { Show() };
-	}, [input]);
+	let [ispending, startTransition] = useTransition();
 
 
-	let Show = () => {
-		console.log("show()")
+	const handleChange = (e) => {
+
+		setinput(e.target.value);
+		let data = [];
 		let count = 0;
 
-		while (count < 1000) {
-			data.push(input)
-			console.log(data);
-			count++;
-		}
-
-		return data.map((x) =>
-			<div className="container border border-danger m-1 d-flex align-content-center">
-				<p className="text-light  d-flex d-flex align-content-center p-0">عدد وارد شده برابر است با: {x}</p>
-			</div>)
+		startTransition(() => {
+			while (count < 1000) {
+				data.push(e.target.value);
+				count++;
+			}
+			setlist(data);
+			console.log("data set()");
+		})
 	}
 
+	const listmap = list.map((x) =>
+		<div className="container border border-danger m-1 d-flex align-content-center">
+			<p className="text-light  d-flex d-flex align-content-center p-0">عدد وارد شده برابر است با: {x}</p>
+		</div>);
+
+	const loading = <div className="container  d-flex justify-content-center"><Spiner></Spiner></div>;
 
 	return (
 		<div className="container">
-			<input type="number" className="form-control" onChange={(e) => setinput(e.target.value)} />
-
-			<div className="container border d-flex justify-content-center py-1 my-1 flex-column">
-				{input ? Show() : <h5>درحال انتظار.....</h5>}
-
+			<input type="number" className="form-control" onChange={handleChange} />
+			<div className="container  d-flex justify-content-center py-1 my-1 flex-column">
+				{input ? (ispending ? loading : listmap) : null}
 			</div>
-
 		</div>
 	)
 };
